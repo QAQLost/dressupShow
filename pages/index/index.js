d40ff4e2638e82f9d667ed740fa2188b482d69df
marve <2829227463@qq.com>
2019-09-27 19:41:36 +0800

    增加一键重置功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,83 +5,83 @@ const app = getApp()
 Page({
   data: {
     TabCur: 0,
-    wardrobe:[{
-      name:'衬衣',
-      type:'cy',
-      num:2,
-      imgShow:true,
-      offset:1,
-      double:true,
-      badge:false  
-    },{
-      name:'马甲',
-      type:'mj',
-      num:1,
-      imgShow:true,
-      offset:1,
-      double:true,
-      badge:false  
-    },{
-      name:'西服',
-      type:'xf',
-      num:7,
-      imgShow:true,
-      offset:1,
-      double:true,
-      badge:false  
-    },{
-      name:'大衣',
-      type:'dy',
-      num:5,
-      imgShow:true,
-      offset:1,
-      double:true,
-      badge:false  
-    },{
-      name:'西裤',
-      type:'xk',
-      num:4,
-      imgShow:true,
-      offset:1,
-      double:true,
-      badge:false  
+    wardrobe: [{
+      name: '衬衣',
+      type: 'cy',
+      num: 2,
+      imgShow: true,
+      offset: 1,
+      double: true,
+      badge: false
+    }, {
+      name: '马甲',
+      type: 'mj',
+      num: 1,
+      imgShow: true,
+      offset: 1,
+      double: true,
+      badge: false
+    }, {
+      name: '西服',
+      type: 'xf',
+      num: 7,
+      imgShow: true,
+      offset: 1,
+      double: true,
+      badge: false
+    }, {
+      name: '大衣',
+      type: 'dy',
+      num: 5,
+      imgShow: true,
+      offset: 1,
+      double: true,
+      badge: false
+    }, {
+      name: '西裤',
+      type: 'xk',
+      num: 4,
+      imgShow: true,
+      offset: 1,
+      double: true,
+      badge: false
     }],
-    list:['cy','mj','xf','dy','xk']
+    list: ['cy', 'mj', 'xf', 'dy', 'xk']
   },
   //事件处理函数
   bingImgChange(e) {
     let type = e.currentTarget.dataset.type;
     let cur = e.currentTarget.dataset.cur;
-    this.resetData(type,cur);
+    this.resetData(type, cur);
   },
-  resetData(type,cur){
+  resetData(type, cur) {
     let flag = false;
     let fs = false;
     let badge = 0;
     let arr = this.data.list;
     let idx = 0;
     arr.forEach(key => {
-      if(key==type){
+      if (key == type) {
         let offset = this.data.wardrobe[idx].offset;
         let double = this.data.wardrobe[idx].double;
         //console.log("cur:"+cur+",offset:"+offset+",double:"+double);
-        if((offset==cur)&&!double){
-          flag=true;
-          fs=true;
+        if ((offset == cur) && !double) {
+          flag = true;
+          fs = true;
         }
-        if(!flag){
-          badge=true;
+        if (!flag) {
+          badge = true;
         }
-        let changeOffset = "wardrobe["+idx+"].offset";
-        let changeImgShow = "wardrobe["+idx+"].imgShow";
-        let changeDouble = "wardrobe["+idx+"].double";
-        let changeBadge= "wardrobe["+idx+"].badge";
+        let changeOffset = "wardrobe[" + idx + "].offset";
+        let changeImgShow = "wardrobe[" + idx + "].imgShow";
+        let changeDouble = "wardrobe[" + idx + "].double";
+        let changeBadge = "wardrobe[" + idx + "].badge";
         this.setData({
-          [changeOffset]:cur,
-          [changeImgShow]:flag,
-          [changeDouble]:fs,
-          [changeBadge]:badge
-        })        
+          [changeOffset]: cur,
+          [changeImgShow]: flag,
+          [changeDouble]: fs,
+          [changeBadge]: badge
+        })
       }
       idx++;
     });
@@ -89,7 +89,24 @@ Page({
   tabSelect(e) {
     this.setData({
       TabCur: e.currentTarget.dataset.id,
-      scrollLeft: (e.currentTarget.dataset.id-1)*60
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
     })
+  },
+  clearAll() {
+    let arr = this.data.wardrobe;
+    let idx = 0;
+    arr.forEach(key => {
+      let changeOffset = "wardrobe[" + idx + "].offset";
+      let changeImgShow = "wardrobe[" + idx + "].imgShow";
+      let changeDouble = "wardrobe[" + idx + "].double";
+      let changeBadge = "wardrobe[" + idx + "].badge";
+      this.setData({
+        [changeOffset]: 1,
+        [changeImgShow]: true,
+        [changeDouble]: true,
+        [changeBadge]: false
+      })
+      idx++;
+    });
   }
 })
