d0b778155a7dd18cd3e4e43c453054baa92a2a4e
marve <2829227463@qq.com>
2019-09-27 15:28:12 +0800

    增加选中显示图标

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,125 +4,76 @@ const app = getApp()
 
 Page({
   data: {
-    cy: true,
-    mj: true,
-    xf: true,
-    dy: true,
-    xk: true,
-    cyOffset: 1,
-    mjOffset: 1,
-    xfOffset: 1,
-    dyOffset: 1,
-    xkOffset: 1,
-    cyShow: true,
-    mjShow: true,
-    xfShow: true,
-    dyShow: true,
-    xkShow: true,
     TabCur: 0,
     wardrobe:[{
       name:'衬衣',
       type:'cy',
-      num:3  
+      num:2,
+      imgShow:true,
+      offset:1,
+      double:true  
     },{
       name:'马甲',
       type:'mj',
-      num:1 
+      num:1,
+      imgShow:true,
+      offset:1,
+      double:true 
     },{
       name:'西服',
       type:'xf',
-      num:7
+      num:7,
+      imgShow:true,
+      offset:1,
+      double:true 
     },{
       name:'大衣',
       type:'dy',
-      num:6
+      num:5,
+      imgShow:true,
+      offset:1,
+      double:true 
     },{
       name:'西裤',
       type:'xk',
-      num:4
-    }]
+      num:4,
+      imgShow:true,
+      offset:1,
+      double:true 
+    }],
+    list:['cy','mj','xf','dy','xk']
   },
   //事件处理函数
   bingImgChange(e) {
     let type = e.currentTarget.dataset.type;
     let cur = e.currentTarget.dataset.cur;
-    let show = e.currentTarget.dataset.show;
+    this.resetData(type,cur);
+  },
+  resetData(type,cur){
     let flag = false;
     let fs = false;
-    if (type == "cy") {
-      let offset = this.data.cyOffset; 
-      let isShow  = this.data.cyShow;   
-      if ((offset == cur)&&show&&!isShow) {
-        flag = true;
-        fs=true;
+    let arr = this.data.list;
+    let idx = 0;
+    arr.forEach(key => {
+      if(key==type){
+        let offset = this.data.wardrobe[idx].offset;
+        let double = this.data.wardrobe[idx].double;
+        //console.log("cur:"+cur+",offset:"+offset+",double:"+double);
+        if((offset==cur)&&!double){
+          flag=true;
+          fs=true;
+        }
+        let changeOffset = "wardrobe["+idx+"].offset";
+        let changeImgShow = "wardrobe["+idx+"].imgShow";
+        let changeDouble = "wardrobe["+idx+"].double";
+        this.setData({
+          [changeOffset]:cur,
+          [changeImgShow]:flag,
+          [changeDouble]:fs
+        })        
       }
-      //console.log("before:"+offset+",cur:"+cur+",hidden:"+flag,",fs:"+fs+",isShow:"+isShow);
-      this.setData({
-        cyOffset: cur,
-        cy: flag,
-        cyShow:fs
-      })
-    }
-    fs=false;
-    if (type == "mj") {
-      let offset = this.data.mjOffset;
-      let isShow  = this.data.mjShow;   
-      if ((offset == cur)&&show&&!isShow) {
-        flag = true;
-        fs=true;
-      }
-      //console.log("before:"+offset+",cur:"+cur+",hidden:"+flag,",fs:"+fs+",isShow:"+isShow);
-      this.setData({
-        mjOffset: cur,
-        mj: flag,
-        mjShow:fs
-      })
-    }
-    fs=false;
-    if (type == "xf") {
-      let offset = this.data.xfOffset;
-      let isShow  = this.data.xfShow;   
-      if ((offset == cur)&&show&&!isShow) {
-        flag = true;
-        fs=true;
-      }
-      //console.log("before:"+offset+",cur:"+cur+",hidden:"+flag,",fs:"+fs+",isShow:"+isShow);
-      this.setData({
-        xfOffset: cur,
-        xf: flag,
-        xfShow:fs
-      })
-    }
-    fs=false;
-    if (type == "dy") {
-      let offset = this.data.dyOffset;
-      let isShow  = this.data.dyShow;   
-      if ((offset == cur)&&show&&!isShow) {
-        flag = true;
-        fs=true;
-      }
-      //console.log("before:"+offset+",cur:"+cur+",hidden:"+flag,",fs:"+fs+",isShow:"+isShow);
-      this.setData({
-        dyOffset: cur,
-        dy: flag,
-        dyShow:fs
-      })
-    }
-    fs=false;
-    if (type == "xk") {
-      let offset = this.data.xkOffset;
-      let isShow  = this.data.xkShow;   
-      if ((offset == cur)&&show&&!isShow) {
-        flag = true;
-        fs=true;
-      }
-      //console.log("before:"+offset+",cur:"+cur+",hidden:"+flag,",fs:"+fs+",isShow:"+isShow);
-      this.setData({
-        xkOffset: cur,
-        xk: flag,
-        xkShow:fs
-      })
-    }
+      idx++;
+    });
   },
   tabSelect(e) {
     this.setData({
